f0244a8b64a6e07f313fa541e92c0007ae577e00
Qing Peng <qing.peng@foohu.com>
2023-05-25 15:22:19 +0800

    merge: add rectangle

--- uic.py
+++ uic.py
@@ -2,15 +2,15 @@
 
 from typing import Optional
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
+    QMetaObject, QObject, QPoint, QRect, Signal,
     QSize, QTime, QUrl, Qt,QItemSelectionModel, QRectF)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient, QPen,
-    QCursor, QFont, QFontDatabase, QGradient,
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient, QPen,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
-    QTransform, QStandardItemModel, QStandardItem)
-from PySide6.QtWidgets import (QApplication, QGraphicsView, QMainWindow, QMenu, QLabel, QGraphicsLineItem,
-    QGraphicsRectItem, QMenuBar, QSizePolicy, QStatusBar, QWidget, QGridLayout, QListView, QListWidget, QGraphicsScene,QGraphicsView)
+    QTransform, QStandardItemModel, QStandardItem, QPolygonF)
+from PySide6.QtWidgets import (QApplication, QGraphicsView, QMainWindow, QMenu, QLabel, QGraphicsLineItem, QGraphicsRectItem, QGraphicsPolygonItem, QGraphicsEllipseItem,
+    QMenuBar, QSizePolicy, QStatusBar, QWidget, QGridLayout, QListView, QListWidget, QGraphicsScene,QGraphicsView,QGraphicsTextItem,QGraphicsItem)
 from PySide6.QtCharts import QChart, QChartView, QLineSeries,QXYSeries,QValueAxis, QLogValueAxis
 from PySide6.QtCore import QPointF
 
@@ -19,28 +19,78 @@ from tools import run_layout
 
 import numpy as np
 
+scale=5
+
+class Polygonf(QGraphicsPolygonItem):
+    def __init__(self, *args, **kwargs):
+        QGraphicsPolygonItem.__init__(self, *args, **kwargs)
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
+
+class Line(QGraphicsLineItem):
+    def __init__(self, *args, **kwargs):
+        QGraphicsLineItem.__init__(self, *args, **kwargs)
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
+
+class Cycle(QGraphicsEllipseItem):
+    def __init__(self, *args, **kwargs):
+        QGraphicsEllipseItem.__init__(self, *args, **kwargs)
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
+
+class Rect(QGraphicsRectItem):
+    def __init__(self, *args, **kwargs):
+        QGraphicsRectItem.__init__(self, *args, **kwargs)
+        self.setFlag(QGraphicsItem.ItemIsSelectable, True)
+
 class ScheScene(QGraphicsScene):
     def __init__(self):
         super().__init__()
         self.symbol = 'NA'
+        self.doDel = False
+        self.cursorSymbol=None
+        self.symbols = []
         self.wireStartPos = None
         self.rectStartPos = None
-
+    
     def mousePressEvent(self, event) -> None:
-        pos = event.scenePos()
-        if self.symbol == 'RR':
-            self.wireStartPos = None
-            self.painRR(event)
-        elif self.symbol == 'WW':
-            self.painWW(event)
-        elif self.symbol == 'RECT':
-            self.painRect(event)
+        if self.doDel:
+            pass
+        else:
+            if self.symbol == 'R':
+                self.wireStartPos = None
+                self.painR(event)
+            elif self.symbol == 'G':
+                self.wireStartPos = None
+                self.painG(event)
+            elif self.symbol == 'V':
+                self.wireStartPos = None
+                self.painV(event)
+            elif self.symbol == 'WW':
+                self.painWW(event)
+            elif self.symbol == 'RECT':
+                self.painRect(event)
+
+        return super().mousePressEvent(event)
+
+
+    def mouseMoveEvent(self, event) -> None:
+        if self.cursorSymbol is None:
+            if self.symbol == 'R':
+                self.painR(event)
+            elif self.symbol == 'G':
+                self.painG(event)
+            elif self.symbol == 'V':
+                self.painV(event)
+        else:
+            for item in self.cursorSymbol:
+                item.setPos(event.scenePos())
 
     def mouseReleaseEvent(self, event):
         if self.symbol == 'RECT':
             self.painRect(event)
 
-    def painRR(self, event):
+    def painR(self, event):
+        self.cursorSymbol = []
+        self.cursorSymbolType='R'
         lines=[[0.000000,375.000000,0.000000,300.000000], 
                [0.000000,300.000000,-62.500000,281.250000], 
                [-62.500000,281.250000,62.500000,243.750000], 
@@ -51,10 +101,93 @@ class ScheScene(QGraphicsScene):
                [62.500000,93.750000,0.000000,75.000000], 
                [0.000000,75.000000,0.000000,0.000000]]
         for line in lines:
-            line = [l/10 for l in line]
-            line = QGraphicsLineItem(*line)
+            line = [l/scale for l in line]
+            line = Line(*line)
             self.addItem(line)
+            self.cursorSymbol.append(line)
             line.setPos(event.scenePos())
+        pin1=Rect(0-20/scale, 375/scale,40/scale,40/scale)
+        self.addItem(pin1)
+        self.cursorSymbol.append(pin1)
+        pin1.setPos(event.scenePos())
+        pin2=Rect(0-20/scale, 0-40/scale,40/scale,40/scale)
+        self.addItem(pin2)
+        self.cursorSymbol.append(pin2)
+        pin2.setPos(event.scenePos())
+
+        name = QGraphicsTextItem('    R1')
+        self.addItem(name)
+        self.cursorSymbol.append(name)
+        #x = event.scenePos().x()
+        #y = event.scenePos().y()
+        name.setPos(event.scenePos())
+
+        self.symbols.append(self.cursorSymbol)
+
+
+    def painG(self, event):
+        self.cursorSymbol = []
+        points=[[-62.500000,187.500000], [62.500000,187.500000], [0.000000,250.000000]]
+        polygonf = QPolygonF()
+        for point in points:
+            polygonf.append(QPointF(point[0]/scale,point[1]/scale))
+        ground = Polygonf(polygonf)
+        self.addItem(ground)
+        self.cursorSymbol.append(ground)
+        ground.setPos(event.scenePos())
+
+        line=[0.000000,0.000000,0.000000,187.500000]
+        line = [l/scale for l in line]
+        line = Line(*line)
+        self.addItem(line)
+        self.cursorSymbol.append(line)
+        line.setPos(event.scenePos())
+
+        pin=Rect(0-20/scale, 0-40/scale,40/scale,40/scale)
+        self.addItem(pin)
+        self.cursorSymbol.append(pin)
+        pin.setPos(event.scenePos())
+        self.symbols.append(self.cursorSymbol)
+
+    def painV(self, event):
+        self.cursorSymbol = []
+        lines = [[0.000000,175.000000,0.000000,137.500000],[18.750000,156.250000,-18.750000,156.250000],[0.000000,125.000000,0.000000,0.000000],[0.000000,250.000000,0.000000,375.000000],[18.750000,218.750000,-18.750000,218.750000]]
+        for line in lines:
+            line = [l/scale for l in line]
+            line = Line(*line)
+            self.addItem(line)
+            self.cursorSymbol.append(line)
+            line.setPos(event.scenePos())
+        cp=[p/scale for p in [0.000000,187.500000,68.750000]]
+        cycle = Cycle(cp[0]-cp[2], cp[1]-cp[2], cp[2]*2,cp[2]*2)
+        self.addItem(cycle)
+        self.cursorSymbol.append(cycle)
+        cycle.setPos(event.scenePos())
+
+        pin1=Rect(0-20/scale, 375/scale,40/scale,40/scale)
+        self.addItem(pin1)
+        self.cursorSymbol.append(pin1)
+        pin1.setPos(event.scenePos())
+        pin2=Rect(0-20/scale, 0-40/scale,40/scale,40/scale)
+        self.addItem(pin2)
+        self.cursorSymbol.append(pin2)
+        pin2.setPos(event.scenePos())
+
+        name = QGraphicsTextItem('    V1')
+        self.addItem(name)
+        self.cursorSymbol.append(name)
+        #x = event.scenePos().x()
+        #y = event.scenePos().y()
+        name.setPos(event.scenePos())
+
+        self.symbols.append(self.cursorSymbol)
+
+    def cleanCursorSymbol(self):
+        if self.cursorSymbol is not None:
+            for item in self.cursorSymbol:
+                self.removeItem(item)
+            self.cursorSymbol=None
+        self.symbol = 'NA'
 
     def painWW(self, event):
         click = event.scenePos()
@@ -69,7 +202,7 @@ class ScheScene(QGraphicsScene):
             else:
                 endPos = [clickX, self.wireStartPos[1]]
             line = self.wireStartPos + endPos
-            line = QGraphicsLineItem(*line)
+            line = Line(*line)
             line.setPen(QColor('blue'))
             self.addItem(line)
             self.wireStartPos = endPos
@@ -109,29 +242,38 @@ class Ui_MainWindow(object):
         self.label = None
         self.scene = None
 
-        self.actionR = QAction(QIcon("r.png"), "&", self)
+        self.actionR = QAction(QIcon("img/r.png"), "&", self)
         self.actionR.setObjectName(u"actionR")
         self.actionR.setShortcut(QKeySequence('r'))
         self.actionR.triggered.connect(self.showR)
 
-        self.actionG = QAction(QIcon("g.png"), "&", self)
+        self.actionG = QAction(QIcon("img/g.png"), "&", self)
         self.actionG.setObjectName(u"actionG")
         self.actionG.setShortcut(QKeySequence('g'))
-        self.actionG.triggered.connect(self.showSchematic)
+        self.actionG.triggered.connect(self.showG)
 
-        self.actionW = QAction(QIcon("w.png"), "&", self)
+        self.actionV = QAction(QIcon("img/v.png"), "&", self)
+        self.actionV.setObjectName(u"actionV")
+        self.actionV.setShortcut(QKeySequence('v'))
+        self.actionV.triggered.connect(self.showV)
+
+        self.actionW = QAction(QIcon("img/w.png"), "&", self)
         self.actionW.setObjectName(u"actionW")
         self.actionW.setShortcut(QKeySequence('w'))
         self.actionW.triggered.connect(self.showW)
 
-        self.actionRect = QAction(QIcon("rect.png"), "&", self)
+        self.actionRect = QAction(QIcon("img/rect.png"), "&", self)
         self.actionRect.setObjectName(u"actionRect")
         self.actionRect.setShortcut(QKeySequence('t'))
         self.actionRect.triggered.connect(self.showRect)
 
+        self.actionDEL = QAction(QIcon("img/del.png"), "&", self)
+        self.actionDEL.setObjectName(u"actionDEL")
+        self.actionDEL.setShortcut(QKeySequence('Del'))
+        self.actionDEL.triggered.connect(self.delSymbol)
+
         self.actionShowWave = QAction(text='Show')
         self.actionShowWave.triggered.connect(self.showwave)
-        # self.actionShowWave.setCheckable(True)
 
         self.actionShowLayout = QAction(text='Open')
         self.actionShowLayout.triggered.connect(self.showlayout)
@@ -169,8 +311,10 @@ class Ui_MainWindow(object):
         self.menuEdit.addSeparator()
         self.menuEdit.addAction(self.actionR)
         self.menuEdit.addAction(self.actionG)
+        self.menuEdit.addAction(self.actionV)
         self.menuEdit.addAction(self.actionW)
         self.menuEdit.addAction(self.actionRect)
+        self.menuEdit.addAction(self.actionDEL)
 
         self.menuWave.addAction(self.actionShowWave)
         self.menuLayout.addAction(self.actionShowLayout)
@@ -184,41 +328,68 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", title, None))
         self.actionR.setText(QCoreApplication.translate("MainWindow", u"Resistor", None))
         self.actionG.setText(QCoreApplication.translate("MainWindow", u"Ground", None))
+        self.actionV.setText(QCoreApplication.translate("MainWindow", u"VoltageSource", None))
         self.actionW.setText(QCoreApplication.translate("MainWindow", u"Wire", None))
         self.actionRect.setText(QCoreApplication.translate("MainWindow", u"Rectangle", None))
+        self.actionDEL.setText(QCoreApplication.translate("MainWindow", u"Delete", None))
         self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
         self.menuEdit.setTitle(QCoreApplication.translate("MainWindow", u"Edit", None))
         self.menuWave.setTitle(QCoreApplication.translate("MainWindow", u"Wave", None))
         self.menuLayout.setTitle(QCoreApplication.translate("MainWindow", u"Layout", None))
 
-    def showSchematic(self, s):
-        if self.scene is None:
-            self.scene = ScheScene()
-            self.scene.setSceneRect(QRectF(0, 0, 500, 500))
-            self.view = QGraphicsView(self.scene)
-            self.MainWindow.setCentralWidget(self.view)
-        self.scene.symbol = 'RR'
+    def createScene(self):
+        self.scene = ScheScene()
+        self.scene.setSceneRect(QRectF(0, 0, 500, 500))
+        self.view = QGraphicsView(self.scene)
+        self.view.setMouseTracking(True)
+        self.MainWindow.setCentralWidget(self.view)
 
     def showR(self,s):
-        if self.label is None:
-            self.label = QLabel()
-            self.canvas = QPixmap(490, 330)
-            self.canvas.fill(Qt.white)
-            self.label.setPixmap(self.canvas)
-            self.MainWindow.setCentralWidget(self.label)
-        self.symbol = "R"
+        if self.scene is None:
+            self.createScene()
+
+        self.scene.cleanCursorSymbol()
+        self.scene.symbol = 'R'
+
+    def delSymbol(self,s):
+        if self.scene is not None:
+            self.scene.cleanCursorSymbol()
+
+        for item in self.scene.selectedItems():
+            item_in_a_symbol = False
+            for symbol in self.scene.symbols:
+                if item in symbol:
+                    item_in_a_symbol = True
+                    for i in symbol:
+                        self.scene.removeItem(i)
+            if not item_in_a_symbol:
+                self.scene.removeItem(item)
+
+
+    def showG(self,s):
+        if self.scene is None:
+            self.createScene()
+
+        self.scene.cleanCursorSymbol()
+        self.scene.symbol = 'G'
+
+    def showV(self,s):
+        if self.scene is None:
+            self.createScene()
+        self.scene.cleanCursorSymbol()
+        self.scene.symbol = 'V'
 
     def showW(self, position):
         if self.scene is None:
-            self.scene = ScheScene()
-            self.scene.setSceneRect(QRectF(0, 0, 500, 500))
-            self.view = QGraphicsView(self.scene)
-            self.MainWindow.setCentralWidget(self.view)
+            self.createScene()
+        self.scene.cleanCursorSymbol()
         self.scene.symbol = 'WW'
-
+    
     def showRect(self, position):
-        self.showSchematic(position)
-        self.scene.symbol = 'RECT'
+        if self.scene is None:
+            self.createScene()
+        self.scene.cleanCursorSymbol()
+        self.scene.symbol = "RECT"
 
     def showwave(self, s):
         #wavefile parse
