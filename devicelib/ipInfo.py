34b39cc2cd2f9d2f0c2d650af3b4df822cfdc76d
Qing Peng <qing.peng@foohu.com>
2023-09-20 14:56:19 +0800

    add ip bandgap

--- devicelib/ipInfo.py
+++ devicelib/ipInfo.py
@@ -5,4 +5,11 @@ Model:LDO
 Descr:低压降稳压器Low-Dropout Regulator
 Pin:vcc,vm,vss,vout
 ENDDEF
+
+DEF BG_ACTIVE
+Type:BANDGAP
+Model:BG_ACTIVE
+Descr:带隙基准源Bandgap Reference
+Pin:PWR,ENABLE,OUT,SGND
+ENDDEF
 """
