b45476f4746b1582d4f4780f4a5f12a49836ec1d
Qing Peng <qing.peng@foohu.com>
2023-12-26 16:23:37 +0800

    improve status message display

--- src/tool/status.py
+++ src/tool/status.py
@@ -1,11 +1,11 @@
 # Status Bar
 statBar = None
-def setStatus(msg, timeout=5000, bar=None):
+def setStatus(msg, bar=None):
     global statBar
     if statBar is None and bar is not None:
         statBar = bar
     statBar.clearMessage()
-    statBar.showMessage(msg, timeout=timeout)
+    statBar.showMessage(msg, timeout=-1)
 
 
 # Tool Bar
